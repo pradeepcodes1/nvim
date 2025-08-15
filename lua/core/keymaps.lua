d96442fe0ff7b822d7f6e00dfc5596e1e3e6175c
Pradeep <219465716+pradeepcodes1@users.noreply.github.com>
2025-08-15 12:27:42 +0530

    Added LazyGit support

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,26 +10,23 @@ map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
 
-
-
 -- BarBar keymaps
 local opts = { noremap = true, silent = true }
-map('n', 'b<Left>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', 'b<Right>', '<Cmd>BufferNext<CR>', opts)
-map('n', 'bk',   '<Cmd>BufferPick<CR>', opts)
-map('n', 'bg1', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', 'bg2', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', 'bg3', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', 'bg4', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', 'bg5', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', 'bg6', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', 'bg7', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', 'bg8', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', 'bg9', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', 'bg0', '<Cmd>BufferLast<CR>', opts)
+map("n", "b<Left>", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "b<Right>", "<Cmd>BufferNext<CR>", opts)
+map("n", "bk", "<Cmd>BufferPick<CR>", opts)
+map("n", "bg1", "<Cmd>BufferGoto 1<CR>", opts)
+map("n", "bg2", "<Cmd>BufferGoto 2<CR>", opts)
+map("n", "bg3", "<Cmd>BufferGoto 3<CR>", opts)
+map("n", "bg4", "<Cmd>BufferGoto 4<CR>", opts)
+map("n", "bg5", "<Cmd>BufferGoto 5<CR>", opts)
+map("n", "bg6", "<Cmd>BufferGoto 6<CR>", opts)
+map("n", "bg7", "<Cmd>BufferGoto 7<CR>", opts)
+map("n", "bg8", "<Cmd>BufferGoto 8<CR>", opts)
+map("n", "bg9", "<Cmd>BufferGoto 9<CR>", opts)
+map("n", "bg0", "<Cmd>BufferLast<CR>", opts)
 
 -- Terminal keymaps
-map('n', '<leader>t', '<Cmd>ToggleTerm<CR>', opts)
-map('n', '<leader>th', '<Cmd>ToggleTerm direction=horizontal<CR>', opts)
-map('n', '<leader>tv', '<Cmd>ToggleTerm direction=vertical<CR>', opts)
-
+map("n", "<leader>t", "<Cmd>ToggleTerm<CR>", opts)
+map("n", "<leader>th", "<Cmd>ToggleTerm direction=horizontal<CR>", opts)
+map("n", "<leader>tv", "<Cmd>ToggleTerm direction=vertical<CR>", opts)
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,29 +1,56 @@
 return {
-  'akinsho/toggleterm.nvim',
-  version = "*", -- or a specific version tag
-  config = function()
-    require('toggleterm').setup({
-      direction = 'float',      
-      float_opts = {
-        border = 'single', -- 'single', 'double', 'shadow', 'curved'
-      },
-    })
+	"akinsho/toggleterm.nvim",
+	version = "*", -- or a specific version tag
+	config = function()
+		require("toggleterm").setup({
+			direction = "float",
+			float_opts = {
+				border = "single", -- 'single', 'double', 'shadow', 'curved'
+			},
+		})
 
-    -- Custom function to set keymaps specifically for terminal buffers
-    function _G.set_terminal_keymaps()
-      local opts = { buffer = 0 }
-      -- Use 'jk' to exit terminal mode
-      vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-      -- Close the terminal with the same key used to open it
-      vim.keymap.set('t', '<leader>t', [[<Cmd>ToggleTerm<CR>]], opts)
-    end
+		-- Custom function to set keymaps specifically for terminal buffers
+		function _G.set_terminal_keymaps()
+			local opts = { buffer = 0 }
+			-- Use 'jk' to exit terminal mode
+			vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+			-- Close the terminal with the same key used to open it
+			vim.keymap.set("t", "<leader>t", [[<Cmd>ToggleTerm<CR>]], opts)
+		end
 
-    -- Use an autocommand to run the function when a terminal is opened
-    vim.api.nvim_create_autocmd('TermOpen', {
-      pattern = 'term://*',
-      callback = function()
-        set_terminal_keymaps()
-      end,
-    })
-  end,
+		-- Use an autocommand to run the function when a terminal is opened
+		vim.api.nvim_create_autocmd("TermOpen", {
+			pattern = "term://*",
+			callback = function()
+				set_terminal_keymaps()
+			end,
+		})
+
+		-- Function to toggle a dedicated lazygit terminal
+		local lazygit_toggle
+		do
+			local Terminal = require("toggleterm.terminal").Terminal
+			local lazygit_term = Terminal:new({
+				cmd = "lazygit",
+				dir = "git_dir", -- Opens in the project's git root
+				hidden = true, -- Hide it on startup
+				direction = "float",
+				float_opts = {
+					border = "double", -- Differentiate it visually
+				},
+				close_on_exit = true,
+				-- When the terminal closes, refresh file status
+				on_close = function(_)
+					vim.cmd("checktime")
+				end,
+			})
+
+			lazygit_toggle = function()
+				lazygit_term:toggle()
+			end
+		end
+
+		-- Map a key to the lazygit toggle function
+		vim.keymap.set("n", "<leader>tg", lazygit_toggle, { desc = "Toggle lazygit" })
+	end,
 }
